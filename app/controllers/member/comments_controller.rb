fb032ff182f39fde738d63b9a536fd45fe0276bf
minht04 <sky.force.1013@gmail.com>
2021-03-06 05:47:04 +0000

    [Add] comment

--- app/controllers/member/comments_controller.rb
+++ app/controllers/member/comments_controller.rb
@@ -1,8 +1,24 @@
 class Member::CommentsController < ApplicationController
-  
+  before_action :authenticate_member!
+
   def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.member_id = current_member.id
+    @comment.save
+    render :index
   end
-  
+
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    render :index
   end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body, :post_id, :member_id)
+  end
+
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment < ApplicationRecord
-  
+
   belongs_to :member
   belongs_to :post
-  
+
+  validates :body, presence: true
+
+
 end
