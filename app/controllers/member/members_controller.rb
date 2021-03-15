95629f4841ff6de33dc9f6b5ceb36888a4d51f16
minht04 <sky.force.1013@gmail.com>
2021-03-15 09:38:12 +0000

    [fix]layout

--- app/controllers/member/members_controller.rb
+++ app/controllers/member/members_controller.rb
@@ -1,4 +1,5 @@
 class Member::MembersController < ApplicationController
+  before_action :authenticate_member!
   before_action :ensure_correct_member, only: [:update, :edit]
   before_action :check_guest, only: [:update]
 
@@ -9,7 +10,7 @@ class Member::MembersController < ApplicationController
     if params[:content] != nil
       @content = params[:content]
       @members = Member.where("name like '%" + params[:content] + "%'").page(params[:page]).reverse_order
-    end    
+    end
   end
 
   def show
@@ -32,11 +33,11 @@ class Member::MembersController < ApplicationController
     @member.update(member_params)
     redirect_to member_path(@member.id)
   end
-  
+
   def exit  # アカウント削除画面に遷移
     @member = current_member
   end
-  
+
   def hide   # アカウント削除
     @member = Member.find(params[:id])
     @member.update(is_deleted: true)
@@ -44,7 +45,7 @@ class Member::MembersController < ApplicationController
     flash[:error] = "アカウントを削除しました。"
     redirect_to root_path
   end
-  
+
 
   def follows # follower一覧
     member = Member.find(params[:id])
