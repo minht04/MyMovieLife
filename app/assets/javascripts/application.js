00f80a3b48dcfc0c27141fef827cffca24059e97
minht04 <sky.force.1013@gmail.com>
2021-03-22 05:38:56 +0000

    refactor:リーダブルコード

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -46,3 +46,17 @@ var mySwiper = new Swiper ('.swiper-container',
     loop: true,
 	});
 });
+
+// aboutスライドイン
+$(function () {
+  $(window).scroll(function () {
+      const wHeight = $(window).height();
+      const scrollAmount = $(window).scrollTop();
+      $('.scrollanime').each(function () {
+          const targetPosition = $(this).offset().top;
+          if(scrollAmount > targetPosition - wHeight + 60) {
+              $(this).addClass("fadeInDown");
+          }
+      });
+    });
+});
