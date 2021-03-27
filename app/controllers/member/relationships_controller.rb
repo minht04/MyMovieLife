6233bfa099dd40111ed045ef2e6d123cca41cd5c
minht04 <sky.force.1013@gmail.com>
2021-03-27 07:36:25 +0000

    [update]フォロー非同期化

--- app/controllers/member/relationships_controller.rb
+++ app/controllers/member/relationships_controller.rb
@@ -5,11 +5,10 @@ class Member::RelationshipsController < ApplicationController
     @member = Member.find(params[:member_id])
     current_member.follow(params[:member_id])
     @member.create_notification_follow!(current_member)
-    redirect_to request.referer
   end
 
   def destroy
+    @member = Member.find(params[:member_id])
     current_member.unfollow(params[:member_id])
-    redirect_to request.referer
   end
 end
