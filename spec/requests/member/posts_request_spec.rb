28e61030f44c145ddbb1b7e8f3ce16f3fbca9d04
minht04 <sky.force.1013@gmail.com>
2021-03-28 11:29:08 +0000

    [update]RSpec posts system

---
+++ spec/requests/member/posts_request_spec.rb
@@ -0,0 +1,85 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  let(:member) { create(:member) }
+  let(:other_member) { create(:member) }
+  let!(:post) { create(:post, member_id: member.id) }
+
+  describe '投稿新規登録画面の表示' do
+    context 'ログインしていない場合' do
+      it "ログインページへリダイレクトすること" do
+        get new_post_path
+        expect(response).to redirect_to new_member_session_path
+      end
+    end
+
+    context 'ログインしている場合' do
+      it 'リクエストが成功すること' do
+        sign_in member
+        get new_post_path
+        expect(response).to have_http_status '200'
+      end
+    end
+  end
+
+  describe '投稿一覧画面の表示' do
+    context 'ログインしていない場合' do
+      it "ログインページへリダイレクトすること" do
+        get posts_path
+        expect(response).to redirect_to new_member_session_path
+      end
+    end
+
+    context 'ログインしている場合' do
+      it 'リクエストが成功すること' do
+        sign_in member
+        get posts_path
+        expect(response).to have_http_status '200'
+      end
+    end
+  end
+
+  describe '投稿詳細画面の表示' do
+    context 'ログインしていない場合' do
+      it "ログインページへリダイレクトすること" do
+        get post_path post.id
+        expect(response).to redirect_to new_member_session_path
+      end
+    end
+
+    context 'ログインしている場合' do
+      it 'リクエストが成功すること' do
+        sign_in member
+        get post_path post.id
+        expect(response).to have_http_status '200'
+      end
+    end
+  end
+
+  describe '投稿編集画面の表示' do
+    context 'ログインしていない場合' do
+      it "ログインページへリダイレクトすること" do
+        get edit_post_path post.id
+        expect(response).to redirect_to new_member_session_path
+      end
+    end
+
+    context 'ログインしている場合' do
+      context '投稿者がログインしている場合' do
+        it 'リクエストが成功すること' do
+          sign_in member
+          get edit_post_path post.id
+          expect(response).to have_http_status '200'
+        end
+      end
+      context '投稿者以外がログインしている場合' do
+        it '投稿一覧に遷移すること' do
+          sign_in other_member
+          get edit_post_path post.id
+          expect(response).to redirect_to posts_path
+        end
+      end
+    end
+  end
+
+end
