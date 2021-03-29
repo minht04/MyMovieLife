229412bce284ffba162723516083c11ee424ef4c
minht04 <sky.force.1013@gmail.com>
2021-03-29 07:11:31 +0000

    [update]admin ユーザー一覧

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -2,7 +2,7 @@ class Admin::MembersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @members = Member.page(params[:page]).reverse_order
+    @members = Member.page(params[:page]).order(created_at: :asc)
   end
 
   def show
